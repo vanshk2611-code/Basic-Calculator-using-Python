e721a57ba8e4286e21d15811dd2c18cafaad0664
Vansh Kulshreshtha <vanshk2611@gmail.com>
2026-05-17 16:47:54 +0530

    Update Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -7,22 +7,25 @@ b= int(input("Enter the second number: "))
 
 x = input("Enter the operation you want to perform ( +, -, *, /, ^, mod, //(for floor division)): ")
 
-if (x == "+"):
-    print("The sum of", a, "and", b, "is", a + b)
-elif (x == "-"):
-    print("The difference of", a, "and", b, "is", a - b)
-elif (x=="*"):
-    print("The product of", a, "and", b, "is", a * b)
-elif (x== "/"):
-    print("The value of", a, "divided by", b, "is", a / b)
-elif (x == "^"):
-    print("The value of", a, "raised to the power", b, "is", a ** b)
-elif (x == "mod"):
-    print("The remainder when", a, "is divided by", b, "is", a % b)
-elif (x == "//"):
-    print("The floor division of", a, "and", b, "is", a // b)
+if (a == int and b == int):
+    if (x == "+"):
+        print("The sum of", a, "and", b, "is", a + b)
+    elif (x == "-"):
+        print("The difference of", a, "and", b, "is", a - b)
+    elif (x=="*"):
+        print("The product of", a, "and", b, "is", a * b)
+    elif (x== "/"):
+        print("The value of", a, "divided by", b, "is", a / b)
+    elif (x == "^"):
+        print("The value of", a, "raised to the power", b, "is", a ** b)
+    elif (x == "mod"):
+        print("The remainder when", a, "is divided by", b, "is", a % b)
+    elif (x == "//"):
+        print("The floor division of", a, "and", b, "is", a // b)
+    else:
+        print("The operation is not valid. Please enter a valid operation.")
 else:
-    print("The operation is not valid. Please enter a valid operation.")
+    print("Enter valid integers.")
 
 # In the above code, we take two numbers as input and then ask the user to enter the operation they want to perform.
 # We use an if-elif-else statement to check which operation the user has chosen and perform the corresponding calculation.
