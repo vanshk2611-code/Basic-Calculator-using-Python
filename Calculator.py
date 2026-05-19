bbd3b9d24b720480b936e83aa9c87c2321c92590
Vansh Kulshreshtha <vanshk2611@gmail.com>
2026-05-19 16:06:02 +0530

    Refactor calculator to loop for multiple calculations

--- Calculator.py
+++ Calculator.py
@@ -2,33 +2,34 @@
 
 print("Hi welcome to Python Calculator-2 \n Made buy :- Vansh Kulshreshtha")
 
-a= int(input("Enter the first number: "))
-b= int(input("Enter the second number: "))
+while True:
+    a= int(input("Enter the first number: "))
+    b= int(input("Enter the second number: "))
 
-x = input("Enter the operation you want to perform ( +, -, *, /, ^, mod, //(for floor division)): ")
+    x = input("Enter the operation you want to perform ( +, -, *, /, ^, mod, //(for floor division)): ")
 
-if (x == "+"):
-    print("The sum of", a, "and", b, "is", a + b)
-elif (x == "-"):
-    print("The difference of", a, "and", b, "is", a - b)
-elif (x=="*"):
-    print("The product of", a, "and", b, "is", a * b)
-elif (x== "/" and b != 0):
-    print("The value of", a, "divided by", b, "is", a / b)
-elif (x == "/" and b == 0):
-    print("Error: Cannot divide by zero!")
-elif (x == "^"):
-    print("The value of", a, "raised to the power", b, "is", a ** b)
-elif (x == "mod" and b != 0):
-    print("The remainder when", a, "is divided by", b, "is", a % b)
-elif (x == "mod" and b == 0):
-    print("Error: Cannot perform modulus by zero!")
-elif (x == "//" and b != 0):
-    print("The floor division of", a, "and", b, "is", a // b)
-elif (x == "//" and b == 0):
-    print("Error: Cannot perform floor division by zero!")
-else:
-    print("The operation is not valid. Please enter a valid operation.")
+    if (x == "+"):
+        print("The sum of", a, "and", b, "is", a + b)
+    elif (x == "-"):
+        print("The difference of", a, "and", b, "is", a - b)
+    elif (x=="*"):
+        print("The product of", a, "and", b, "is", a * b)
+    elif (x== "/" and b != 0):
+        print("The value of", a, "divided by", b, "is", a / b)
+    elif (x == "/" and b == 0):
+        print("Error: Cannot divide by zero!")
+    elif (x == "^"):
+        print("The value of", a, "raised to the power", b, "is", a ** b)
+    elif (x == "mod" and b != 0):
+        print("The remainder when", a, "is divided by", b, "is", a % b)
+    elif (x == "mod" and b == 0):
+        print("Error: Cannot perform modulus by zero!")
+    elif (x == "//" and b != 0):
+        print("The floor division of", a, "and", b, "is", a // b)
+    elif (x == "//" and b == 0):
+        print("Error: Cannot perform floor division by zero!")
+    else:
+        print("The operation is not valid. Please enter a valid operation.")
 
 
 # In the above code, we take two numbers as input and then ask the user to enter the operation they want to perform.
